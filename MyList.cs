e6ef6e537270d0242e87821bf51e86bd72270c1c
Mehmet ORTABURUN <mehmet.ortaburun@gmail.com>
2022-02-23 17:27:26 +0300

    Add project files.

---
+++ MyList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    // MyList<T> şeklinde yarazak bu işaretler arasında istediğimiz bir veri tipini tutabiliriz.
+    // T işareti veri tipini simgeler
+    class MyList<T>
+    {
+        // ekeleme fonksiyonunu yazarsak parametre olarak T vermemiz gerekir .
+        // bir dizi referansı tutalım. Tutualacak dizinin tipini T belirler. listeyi new ile belirlediğimizde dizi oluşturulmalı
+        // bunun için ctor ile constructor tnaımlarız
+        T[] items;
+
+        //Constructor tnaımladık. 
+        public MyList()
+        {
+            // sıfır elemanlı bir dizi tanımladık 
+            items = new T[0];
+
+        }
+        public void Add(T item)
+        {
+            // eklenecek olan isimleri tutmamız gerekir. bunun için bir dizi yönetir
+            // Dizi Constructor ile new olurken eleman sayısı sıfırdır.
+
+            // eleman eklemek istersek dizinin eleman sayısını +1 artırmak gerekir bunun için de length kullanırız
+            // dizi eleman sayısını arttırma işlemi yaptığımızda ilk tanımlanan dizinin referans adresi yok olur 
+            //Bunun için geçici bir dizide bunu tutmak gerekir.
+            T[] tempArray = items; // items dizisinin referans numarsını tempArray tutacak 
+            items = new T[items.Length + 1]; // dizimizin eleman sayısını +1 artırır.
+
+            // tempArray deki elemanları tekrar alalım
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i]; // 
+
+            }
+
+            // son eklenecek olan elemanı itemss dizisine ekleyelim
+            items[items.Length - 1] = item;  // items.Length - 1 demek dizi 6 elemanlı ise 0 dan başladığı için 5.yere ekle 
+
+        }
+
+        public int Length
+        {
+            get { return items.Length; }
+        }
+
+        public T[] Item
+        {
+            get { return items; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // kendi llistemizi oluşturralım.MyList generic bir class olduğunda o şekilde tanımlarız. 
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("mehmet");
+            isimler.Add("ahmet");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("kamil");
+
+            Console.WriteLine(isimler.Length);
+
+
+            foreach (var item in isimler.Item)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
